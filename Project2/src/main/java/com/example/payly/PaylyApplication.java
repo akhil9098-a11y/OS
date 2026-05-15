4614cc6f7cdbb5c8bcd4e459774e54f5add0bf85
akhil9098-a11y <akhilgaming9098@gmail.com>
2026-05-15 15:56:13 +0530

    Add files via upload

---
+++ Project2/src/main/java/com/example/payly/PaylyApplication.java
@@ -0,0 +1,13 @@
+package com.example.payly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaylyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaylyApplication.class, args);
+    }
+
+}
